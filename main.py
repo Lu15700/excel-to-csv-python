bfb0a62690cba341698f7405923689539a563d59
Luis <102251361+Lu15700@users.noreply.github.com>
2024-02-18 18:32:32 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pandas as pd
 
-def excel_to_csv(input_file, output_path):
-  read_file = pd.read_excel (r'input_file')
-  transformed_file = read_file.to_csv (r'{output_path}\transformed_file.csv', index = None, header=True)
+def excel_to_csv(input_path, output_path):
+  read_file = pd.read_excel(input_path)
+  
+  transformed_file = read_file.to_csv (fr'{output_path}\transformed_file.csv',
+                                       index= None,
+                                       header= True)
 
-excel_to_csv("INPUT FILE NAME HERE", "OUTPUT PATH HERE")
+excel_to_csv(r"<INPUT PATH>",
+             r"<OUTPUT PATH>")
